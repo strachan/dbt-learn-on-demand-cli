f1330a7b94568c3f14a8927b398b47ec53d666bc
Guilherme Strachan <guilherme@blueorange.digital>
2021-07-22 18:59:33 -0400

    :sparkles: Added bash script to initialize dbt learn

---
+++ dbt-up.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Create folder for dataset
+mkdir ./dataset
+
+# Download the dataset
+curl http://dbt-tutorial-public.s3-us-west-2.amazonaws.com/jaffle_shop_orders.csv --output ./dataset/shop_orders.csv
+
+curl http://dbt-tutorial-public.s3-us-west-2.amazonaws.com/jaffle_shop_customers.csv --output ./dataset/shop_customers.csv
+
+curl http://dbt-tutorial-public.s3-us-west-2.amazonaws.com/stripe_payments.csv --output ./dataset/stripe_payments.csv
+
+# Get the path to the directory containing this script
+PROJECTDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# Get the Docker directory
+DOCKERDIR="${PROJECTDIR}/docker"
+
+# Move to the docker directory
+cd "${DOCKERDIR}"
+
+# Local dbt learn project directory
+export LOCAL_DBT_PATH="${LOCAL_DBT_PATH:=${PROJECTDIR}/dbt_learn}"
+
+# Launch the containers
+docker-compose \
+--project-name dbt-learn \
+up \
+--detach
